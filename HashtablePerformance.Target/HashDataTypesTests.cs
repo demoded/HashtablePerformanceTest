cb8b0eb6a9a2d0b53377863911a8f88659eb97bd
Sergey Idelson <aka.konor@gmail.com>
2021-10-12 07:52:56 +1300

    Refactor tests

---
+++ HashtablePerformance.Target/HashDataTypesTests.cs
@@ -0,0 +1,92 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HashtablePerformance.Target
+{
+    [MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.Net472, baseline: true)]
+    [SimpleJob(RuntimeMoniker.Net48)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [SimpleJob(RuntimeMoniker.Net50)]
+    [SimpleJob(RuntimeMoniker.Net60)]
+    [RPlotExporter]
+    public class HashDataTypesTests
+    {
+        private readonly static Random _rand = new Random(42);
+        private List<string> source;
+
+        [Params(10_000_000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            source = Enumerable.Range(0, N).Select(_ => _rand.Next(N).ToString()).ToList();
+        }
+
+        [Benchmark]
+        public Hashtable AddUniqueToHashTableNew()
+        {
+            var hashTable = new Hashtable(source.Count, 1.0f);
+
+            for (var i = 0; i > source.Count; ++i)
+            {
+                if (hashTable[source[i]] is null)
+                {
+                    hashTable.Add(source[i], source[i]);
+                }
+            }
+
+            return hashTable;
+        }
+
+        [Benchmark]
+        public Hashtable AddUniqueToHashTable()
+        {
+            Hashtable hashTable = new Hashtable();
+
+            foreach (var s in source)
+            {
+                if (hashTable[s] == null)
+                {
+                    hashTable.Add(s, s);
+                }
+            }
+
+            return hashTable;
+        }
+
+        [Benchmark]
+        public Dictionary<string, string> AddUniqueToDictionary()
+        {
+            var dictionary = new Dictionary<string, string>();
+
+            foreach (var s in source)
+            {
+                if (!dictionary.ContainsKey(s))
+                {
+                    dictionary.Add(s, s);
+                }
+            }
+
+            return dictionary;
+        }
+
+        [Benchmark]
+        public HashSet<string> AddUniqueToHashSet()
+        {
+            var hashSet = new HashSet<string>();
+
+            foreach (var s in source)
+            {
+                hashSet.Add(s);
+            }
+
+            return hashSet;
+        }
+    }
+}
--- HashtablePerformanceTest/Program.cs
+++ HashtablePerformanceTest/Program.cs
@@ -1,75 +1,19 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
+using HashtablePerformance.Target;
 
 namespace HashtablePerformanceTest
 {
-    [MemoryDiagnoser]
-    public class HashDataTypes
+    public static class Program
     {
-
-        private static Random _rand = new Random(1);
-        List<string> source = Enumerable.Range(0, 10000000).Select(i => _rand.Next(100000000).ToString()).ToList();
-
-        [Benchmark]
-        public Hashtable AddUniqueToHashTable()
+        public static void Main()
         {
-            Hashtable hashTable = new Hashtable();
-
-            foreach (var s in source)
-            {
-                if (hashTable[s] == null)
-                {
-                    hashTable.Add(s, s);
-                }
-            }
-
-            return hashTable;
-        }
-
-        [Benchmark]
-        public Dictionary<string, string> AddUniqueToDictionary()
-        {
-            Dictionary<string, string> dictionary = new Dictionary<string, string>();
-
-            foreach (var s in source)
-            {
-                if (!dictionary.ContainsKey(s))
-                {
-                    dictionary.Add(s, s);
-                }
-            }
-
-            return dictionary;
-        }
-
-        [Benchmark]
-        public HashSet<string> AddUniqueToHashSet()
-        {
-            HashSet<string> hashSet = new HashSet<string>();
-
-            foreach (var s in source)
-            {
-                hashSet.Add(s);
-            }
-
-            return hashSet;
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
-
-            //var run = new HashDataTypes();
-            //var a = run.AddUniqueToHashTable();
-            //var b = run.AddUniqueToDictionary();
-            //var c = run.AddUniqueToHashSet();
+#if RELEASE
+            _ = BenchmarkRunner.Run(typeof(HashDataTypesTests).Assembly);
+#endif
+#if DEBUG
+            var test = new HashDataTypesTests();
+            var result = test.AddUniqueToHashTableNew();
+#endif
         }
     }
 }
